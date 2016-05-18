75255ff5feb73ffbbf035f87106ce4b6a4dea975
olvit <olvitov@gmail.com>
2016-05-18 16:38:44 +0300

    Сделан вид

---
+++ views/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<table border="1" width="800px">
+    <tr><th>Заголовок</th>
+        <th>Дата</th>
+        <th>Описание</th>
+        <th>Текст</th>
+        <th>Автор</th>
+
+<?php foreach ($items as $item):?>
+
+
+        <tr>
+        <td><?php echo $item->title; ?></td>
+        <td><?php echo $item->data; ?></td>
+        <td><?php echo $item->description; ?></td>
+        <td><?php echo $item->text; ?></td>
+        <td><?php echo $item->author; ?></td>
+
+
+        </tr>
+
+
+    <?php endforeach; ?>
+
+
+    </table>
+
+
+
+
+</body>
+</html>
