dfafe4f8f7aa53826debaf5379a2bdd81fa34b73
olvit <olvitov@gmail.com>
2016-05-19 10:03:59 +0300

    Добавлен ActionOne

--- controllers/ArticlesController.php
+++ controllers/ArticlesController.php
@@ -1,15 +1,23 @@
 <?php
 
-require_once __DIR__ .  '/../models/Articles.php';
+ require_once __DIR__ .  '/../models/Articles.php';
 
 class ArticlesController
 {
-    public function actonAll() {
+    public function actionAll() {
 
         $items = Articles::getAll();
 
-        include __DIR__ . '/../views/index.php';
+        include __DIR__ . '/../views/articles/all.php';
 
+    }
+
+    public function actionOne() {
+
+
+        $id = $_GET['id'];
+        $item = Articles::getOne($id);
+        include __DIR__ . '/../views/articles/one.php';
     }
 
 }
