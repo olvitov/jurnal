102462f1b7a45f91476e3fe51797095025940ef4
olvit <olvitov@gmail.com>
2016-05-25 00:07:39 +0300

    24.05.16

---
+++ classes/IModel.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: olvit
+ * Date: 23.05.2016
+ * Time: 23:25
+ */
+interface IModel
+{
+    public static function getAll();
+    public static function getOne($id);
+
+}
