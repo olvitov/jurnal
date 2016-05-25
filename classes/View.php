c6d0643716ec1ff002ffeea1a9d7763e5716201c
olvit <olvitov@gmail.com>
2016-05-25 18:50:12 +0300

    Конец 4 урока

--- classes/View.php
+++ classes/View.php
@@ -22,16 +22,23 @@ implements Iterator
 
     }
 
-    public function display($template) {
+    public function render($template) {
 
         foreach ($this->data as $key => $val) {
             $$key = $val;
         }
+        ob_start();
         include __DIR__ . '/../views/' . $template;
-        
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
 
     }
 
+    public function display($template) {
+
+        echo $this->render($template);
+    }
 
     public function current()
     {
