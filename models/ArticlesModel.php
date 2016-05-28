47a9e1869c71fb9e964d66eface8f4a05b4f7cff
olvit <olvitov@gmail.com>
2016-05-28 15:47:50 +0300

    PDO

---
+++ models/ArticlesModel.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: olvit
+ * Date: 28.05.2016
+ * Time: 14:26
+ */
+class ArticlesModel
+extends AbstractModel
+{
+    protected static $table = 'articles';
+    
+
+}
---
+++ models/LessonsModel.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: olvit
+ * Date: 28.05.2016
+ * Time: 14:26
+ */
+class LessonsModel
+extends AbstractModel
+{
+    protected static $table = 'lessons';
+
+}
